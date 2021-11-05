6048e559275cc29196537bdd357ca2422b714022
Dhanush Patel <dhanushpatel101@gmail.com>
2021-11-05 16:05:36 -0700

    added boilerplate components

---
+++ src/components/App.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+
+import { ethers } from 'ethers';
+
+// import SwapAIArtifact from "../contracts/SwapAI.json";
+// import contractAddress from "../contracts/contract-address.json";
+
+// import MockSwapAIArtifact from "../contracts/MockSwapAISwap.json";
+// import mockContractAddress from "../contracts/mock-contract-address.json";
+
+import { NoWalletDetected } from './NoWalletDetected';
+import { ConnectWallet } from './ConnectWallet';
+
+// list of network ids https://docs.metamask.io/guide/ethereum-provider.html#properties
+const HARDHAT_NETWORK_ID = '31337';
+const MAINNET_ID = '1';
+const KOVAN_ID = '42';
+const NETWORK_ERR_MSG = 'Please connect Metamask to Localhost:8545, mainnet, or Kovan';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      selectedAddress: undefined,
+      transactionError: undefined,
+      networkError: undefined,
+      optInStatus: false,
+      blockchainMessages: []
+    };
+
+    this.state = this.initialState;
+  }
+
+  async intializeEthers() {
+    // We first initialize ethers by creating a provider using window.ethereum
+    this.provider = new ethers.providers.Web3Provider(window.ethereum);
+    // TODO: uncomment once created mock contract
+    // if (window.ethereum.networkVersion === HARDHAT_NETWORK_ID) {
+    //   this.swapAI = new ethers.Contract(
+    //     mockContractAddress.MockSwapAI,
+    //     MockSwapAIArtifact.abi,
+    //     this.provider.getSigner(0)
+    //   );
+    // }
+    if (window.ethereum.networkVersion === KOVAN_ID) {
+      // TODO: uncomment once added contract abi to this repo
+      // this.swapAI = new ethers.Contract(
+      //   contractAddress.SwapAI,
+      //   SwapAIArtifact.abi,
+      //   this.provider.getSigner(0)
+      // );
+      // console.log("SwapAI Kovan address", contractAddress);
+    }
+    console.log("networkVersion", window.ethereum.networkVersion);
+  }
+
+  dismissNetworkError() {
+    this.setState({ networkError: undefined });
+  }
+
+  async initialize(userAddress) {
+    // This method initializes the dapp
+
+    // We first store the user's address in the component's state
+    this.setState({
+      selectedAddress: userAddress
+    });
+
+    // Then, we initialize ethers
+    await this.intializeEthers();
+  }
+
+  async connectWallet() {
+    //connects dapp to wallet when user clicks on connect wallet button
+
+    const [selectedAddress] = await window.ethereum.enable();
+    console.log('selectedAddress',selectedAddress);
+    // Once we have the address, we can initialize the application.
+
+    if (!this.checkNetwork()) {
+      return;
+    }
+
+    this.initialize(selectedAddress);
+
+    // We reinitialize it whenever the user changes their account.
+    window.ethereum.on('accountsChanged', ([newAddress]) => {
+      // `accountsChanged` event can be triggered with an undefined newAddress.
+      // This happens when the user removes the Dapp from the "Connected
+      // list of sites allowed access to your addresses" (Metamask > Settings > Connections)
+      // To avoid errors, we reset the dapp state
+      if (newAddress === undefined) {
+        return this.resetState();
+      }
+
+      this.initialize(newAddress);
+    });
+
+    // We reset the dapp state if the network is changed
+    window.ethereum.on('networkChanged', () => {
+      this.resetState();
+    });
+  }
+
+  // This method resets the state
+  resetState() {
+    this.setState(this.initialState);
+  }
+
+  // This method checks if Metamask selected network is Localhost:8545, mainnet, or Kovan
+  checkNetwork() {
+    if (window.ethereum.networkVersion === HARDHAT_NETWORK_ID || window.ethereum.networkVersion === MAINNET_ID || window.ethereum.networkVersion === KOVAN_ID) {
+      return true;
+    }
+
+    this.setState({
+      networkError: NETWORK_ERR_MSG,
+    });
+
+    return false;
+  }
+
+  render() {
+    // Ethereum wallets inject the window.ethereum object. If it hasn't been
+    // injected, we instruct the user to install MetaMask.
+    if (window.ethereum === undefined) {
+      return <NoWalletDetected />;
+    }
+
+    if (!this.state.selectedAddress) {
+      return (
+        <ConnectWallet
+          connectWallet={() => this.connectWallet()}
+          networkError={this.state.networkError}
+          dismiss={() => this.dismissNetworkError()}
+        />
+      );
+    }
+
+    return (<div>Hello!</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NetworkErrorMessage.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+export function NetworkErrorMessage({ message, dismiss }) {
+  return (
+    <div className="alert alert-danger" role="alert">
+      {message}
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+        onClick={dismiss}
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+}
